c1bf47c7c0abe0322c052ce9ef370ef205e3b7c0
Miguel Monteiro Claveri <miguelmonteiroclaveri@gmail.com>
2024-11-01 13:12:01 +0100

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# Update the package list
+# Update and upgrade
 sudo apt update
-
-# Upgrade existing packages
 sudo apt upgrade -y
 
-# Install main application requirements
-sudo apt install -y python3-pip python3-asyncio python3-rpi.gpio
+# Install system packages with apt
+sudo apt install -y python3-pip python3-rpi.gpio python3-asyncio 
 
-# Install Azure IoT Device SDK for Python
-pip install azure-iot-device
+# Create a virtual environment
+python3 -m venv my_venv
+source my_venv/bin/activate
 
-# Install Flask API requirements
-pip install flask flask-cors
+# Install remaining packages with pip in the virtual environment
+pip install azure-iot-device flask flask-cors black
 
-# Install formatting tool
-pip install black
+deactivate  # Deactivate the virtual environment when done
